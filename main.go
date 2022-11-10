d20720f566b40067d6ff0d92706f91a3c977f673
Waynel65 <waynel@bu.edu>
2022-11-10 14:45:51 -0500

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+
+/* 
+	backtick notation:
+	- e.g. `json:"name"`
+	- This allows us to map each field to a different name when we send them as responses, 
+	since JSON and Go have different naming conventions.
+*/
+
+type Recipe struct{
+	Name		 string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
 
 func main() {
 	router := gin.Default()
