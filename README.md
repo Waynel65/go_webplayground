b0c28fe4c10b7646db0ce7a72ce6b6838069fece
Waynel65 <waynel@bu.edu>
2022-11-10 22:02:41 -0500

    new recipe endpoint

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# go_webplayground
+# go_webplayground_notes
+
+### Go module
+- automatically records the dependecies you have installed
+- run `go mod init <root_directory_name>` in root directory to intialize the go.mod file
+- on other developers' end run `go mod download` to download the required dependencies
--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
+	"github.com/rs/xid"
+
+	"github.com/gin-gonic/gin"
 )
 
-
-/* 
+/*
 	backtick notation:
 	- e.g. `json:"name"`
-	- This allows us to map each field to a different name when we send them as responses, 
+	- This allows us to map each field to a different name when we send them as responses,
 	since JSON and Go have different naming conventions.
 */
 
 type Recipe struct{
+	ID			 string    `json: "id"`
 	Name		 string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -21,8 +24,28 @@ type Recipe struct{
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipes []Recipe // a global variable that stores the list of recipes
+func init(){
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHandler(c * gin.Context){
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	recipe.ID = xid.New().String() // xid generates a random id
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe) // structure the response as json
+}
+
 
 func main() {
 	router := gin.Default()
-	router.Run()
+	router.POST("/recipes", NewRecipeHandler) // direct post request towards route /recipes to NewRecipeHandler
+	router.Run("localhost:8080")
 }
